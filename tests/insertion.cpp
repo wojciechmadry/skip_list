ffd4e5cfcf3fefd6810747d1dec02b4e0acf38ec
Wojciech Madry <madrywojciech99@gmail.com>
2024-04-05 22:52:01 +0200

    [Bazel] Init bazel/dazel repo
    Signed-off-by: Wojciech Madry <madrywojciech99@gmail.com>

---
+++ tests/insertion.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include "skip_list.hpp"
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect equality.
+  EXPECT_EQ(get_skip(), 5);
+}
+
