33a8105aa2f3b3d03159f3ac983f1f26519e1db7
Wojciech Madry <madrywojciech99@gmail.com>
2024-04-11 21:41:02 +0200

    [Compile commands] Generate compile commands with herdon repo [clang-format] Format code with clang-format/llvm and aspect_rules_lint
    Signed-off-by: Wojciech Madry <madrywojciech99@gmail.com>

--- tests/insertion.cpp
+++ tests/insertion.cpp
@@ -6,24 +6,24 @@
 #include "skip_list.hpp"
 
 class InsertionPushEmplace
-    : public ::testing::TestWithParam<std::function<skip_list<int>&(int)>> {};
+    : public ::testing::TestWithParam<std::function<skip_list<int> &(int)>> {};
 
 TEST_P(InsertionPushEmplace, InsertData) {
   auto func = GetParam();
-  auto& sl = func(2);
+  auto &sl = func(2);
   ASSERT_EQ(sl.front(), 2);
   func(3);
   ASSERT_EQ(sl.front(), 2);
   // func(1);
 }
 
-auto lambda_push = [](int value) -> skip_list<int>& {
+auto lambda_push = [](int value) -> skip_list<int> & {
   static skip_list<int> sl;
   sl.push(value);
   return sl;
 };
 
-auto lambda_emplace = [](int value) -> skip_list<int>& {
+auto lambda_emplace = [](int value) -> skip_list<int> & {
   static skip_list<int> sl;
   sl.emplace(value);
   return sl;
