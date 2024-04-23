f48083d4644dad44afac29b5d44fb63c35185dd3
Wojciech Madry <madrywojciech99@gmail.com>
2024-04-23 20:34:29 +0200

    [Clearing] Test clear function and destructor
    Signed-off-by: Wojciech Madry <madrywojciech99@gmail.com>

--- tests/clearing.cpp
+++ tests/clearing.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <exception>
 #include <new>
+#include <random>
 static std::size_t new_calls = 0;
 static std::size_t delete_calls = 0;
 // no inline, required by [replacement.functions]/3
@@ -49,26 +50,89 @@ void operator delete[](void *ptr, std::size_t size) noexcept {
 #include "skip_list.hpp"
 using namespace sl;
 
-TEST(Clearing, Destructor) {}
+TEST(Clearing, Destructor) {
+  static constexpr std::size_t NEW_SIZE = 500u;
+  std::seed_seq seed{555};
+  delete_calls = 0;
+  new_calls = 0;
+  {
+    skip_list<int> sl;
+    ASSERT_NO_THROW(sl.set_seed(seed));
+    for (std::size_t i = 1u; i <= NEW_SIZE; ++i) {
+      ASSERT_NO_THROW(sl.push(i));
+      ASSERT_EQ(new_calls, i);
+    }
+    ASSERT_EQ(delete_calls, 0);
+  }
+  ASSERT_EQ(delete_calls, NEW_SIZE);
+}
+
 TEST(Clearing, ClearFunction) {
   skip_list<int> sl;
+  std::seed_seq seed{333};
+  ASSERT_NO_THROW(sl.set_seed(seed));
   new_calls = 0;
   delete_calls = 0;
-  sl.push(5);
+  ASSERT_NO_THROW(sl.push(5));
   ASSERT_EQ(new_calls, 1);
   ASSERT_EQ(delete_calls, 0);
   ASSERT_EQ(sl.size(), 1);
-  sl.clear();
+  ASSERT_NO_THROW(sl.clear());
   ASSERT_TRUE(sl.empty());
   ASSERT_EQ(delete_calls, new_calls);
-  sl.push(3);
+  ASSERT_NO_THROW(sl.push(3));
   ASSERT_EQ(new_calls, 2);
   ASSERT_EQ(delete_calls, 1);
   ASSERT_EQ(sl.size(), 1);
-  sl.push(5);
+  ASSERT_NO_THROW(sl.push(5));
   ASSERT_EQ(sl.size(), 2);
   ASSERT_EQ(new_calls, 3);
-  sl.clear();
+  ASSERT_NO_THROW(sl.clear());
   ASSERT_TRUE(sl.empty());
   ASSERT_EQ(delete_calls, new_calls);
+  static constexpr std::size_t NEW_SIZE = 10u;
+  for (std::size_t i = 0u; i < NEW_SIZE; ++i) {
+    ASSERT_NO_THROW(sl.push(i));
+    ASSERT_EQ(new_calls, 4u + i);
+  }
+  ASSERT_EQ(new_calls, 13u);
+  ASSERT_EQ(delete_calls, 3u);
+  ASSERT_NO_THROW(sl.clear());
+  ASSERT_EQ(delete_calls, new_calls);
+}
+
+TEST(Clearing, ClearFunction1000Elements) {
+  skip_list<int> sl;
+  std::seed_seq seed{500};
+  ASSERT_NO_THROW(sl.set_seed(seed));
+  new_calls = 0;
+  delete_calls = 0;
+  static constexpr std::size_t NEW_SIZE = 1000u;
+  for (std::size_t i = 1u; i <= NEW_SIZE; ++i) {
+    ASSERT_NO_THROW(sl.push(i));
+    ASSERT_EQ(new_calls, i);
+  }
+  ASSERT_EQ(new_calls, NEW_SIZE);
+  ASSERT_EQ(delete_calls, 0u);
+  ASSERT_NO_THROW(sl.clear());
+  ASSERT_EQ(delete_calls, new_calls);
+}
+
+TEST(Clearing, ClearFunctionRandomSeed) {
+  for (int i = 0; i <= 300; ++i) {
+    skip_list<int> sl;
+    std::seed_seq seed{i};
+    ASSERT_NO_THROW(sl.set_seed(seed));
+    new_calls = 0;
+    delete_calls = 0;
+    static constexpr std::size_t NEW_SIZE = 10u;
+    for (std::size_t i = 1u; i <= NEW_SIZE; ++i) {
+      ASSERT_NO_THROW(sl.push(i));
+      ASSERT_EQ(new_calls, i);
+    }
+    ASSERT_EQ(new_calls, NEW_SIZE);
+    ASSERT_EQ(delete_calls, 0u);
+    ASSERT_NO_THROW(sl.clear());
+    ASSERT_EQ(delete_calls, new_calls);
+  }
 }
