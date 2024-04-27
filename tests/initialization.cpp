b7093b472f8337b254caadc39089941d94bba6d8
Wojciech Madry <madrywojciech99@gmail.com>
2024-04-27 13:13:50 +0200

    [Constructors] Create and test skip_list constructors
    Signed-off-by: Wojciech Madry <madrywojciech99@gmail.com>

--- tests/initialization.cpp
+++ tests/initialization.cpp
@@ -44,4 +44,124 @@ TEST(Initialization, EmptyList) {
   ASSERT_TRUE(const_sl.empty());
   ASSERT_EQ(sl.size(), 0);
   ASSERT_EQ(const_sl.size(), 0);
+  ASSERT_EQ(const_sl.max_size(),
+            std::numeric_limits<skip_list<int>::difference_type>::max());
+}
+
+TEST(Initialization, ConstructorsSimple) {
+  skip_list<int> sl_def;
+  ASSERT_EQ(sl_def.begin(), nullptr);
+  ASSERT_EQ(sl_def.end(), nullptr);
+  ASSERT_EQ(sl_def.size(), 0u);
+  std::allocator<int> alloc;
+  std::less<int> comp;
+  skip_list<int> sl_alloc(comp, alloc);
+  const auto is_same_allocator =
+      std::is_same_v<decltype(sl_alloc.get_allocator()),
+                     skip_list<int>::allocator_type>;
+  ASSERT_TRUE(is_same_allocator);
+  skip_list<int> sl_count_n(10, 5);
+  ASSERT_EQ(sl_count_n.size(), 10u);
+  ASSERT_EQ(sl_count_n.front(), 5);
+  ASSERT_EQ(sl_count_n.back(), 5);
+  {
+    std::size_t count = 0u;
+    for (auto v : sl_count_n) {
+      ASSERT_EQ(v, 5);
+      ++count;
+    }
+    ASSERT_EQ(count, 10);
+  }
+  skip_list<int> sl_count(7);
+  ASSERT_EQ(sl_count.size(), 7u);
+  ASSERT_EQ(sl_count.front(), int{});
+  ASSERT_EQ(sl_count.back(), int{});
+  {
+    std::size_t count = 0u;
+    for (auto v : sl_count) {
+      ASSERT_EQ(v, int{});
+      ++count;
+    }
+    ASSERT_EQ(count, 7);
+  }
+}
+
+TEST(Initialization, ConstructorIterators) {
+  std::vector<int> v{5, 2, 1, 3, 0, 4};
+  skip_list<int> sl_copy_from;
+  for (int i = -5; i <= 5; ++i) {
+    ASSERT_NO_THROW(sl_copy_from.push(i));
+  }
+  skip_list<int> first_sl(v.begin(), v.end());
+  ASSERT_EQ(v.size(), first_sl.size());
+  auto first_it = first_sl.begin();
+  for (int i = 0; i <= 5; ++i, first_it++) {
+    ASSERT_NE(first_it, nullptr);
+    ASSERT_EQ(*first_it, i);
+  }
+  skip_list<int> second_sl(sl_copy_from.begin(), sl_copy_from.end());
+  ASSERT_EQ(sl_copy_from.size(), second_sl.size());
+  auto second_it = second_sl.begin();
+  for (auto v : sl_copy_from) {
+    ASSERT_NE(second_it, nullptr);
+    ASSERT_EQ(v, *second_it);
+    ++second_it;
+  }
+}
+
+TEST(Initialization, ConstructorsCopy) {
+  skip_list<int> sl;
+  for (int i = -5; i <= 5; ++i) {
+    sl.push(i);
+  }
+  skip_list<int> copy_sl(sl);
+  ASSERT_EQ(copy_sl.size(), sl.size());
+  auto it = copy_sl.begin();
+  for (const auto &v : sl) {
+    ASSERT_NE(it, nullptr);
+    ASSERT_NE(&v, &(*it));
+    ASSERT_EQ(v, *it);
+    ++it;
+  }
+  const auto size = sl.size();
+  const auto &front = sl.front();
+  const auto &back = sl.back();
+  const auto begin = sl.begin();
+  skip_list<int> moved_sl(std::move(sl));
+  ASSERT_EQ(moved_sl.size(), size);
+  ASSERT_EQ(&moved_sl.front(), &front);
+  ASSERT_EQ(&moved_sl.back(), &back);
+  ASSERT_EQ(moved_sl.begin(), begin);
+  it = moved_sl.begin();
+  for (int i = -5; i <= 5; ++i, ++it) {
+    ASSERT_NE(it, nullptr);
+    ASSERT_EQ(*it, i);
+  }
+  // sl is not a valid until clear
+  ASSERT_NO_THROW(sl.clear());
+  ASSERT_TRUE(sl.empty());
+  ASSERT_EQ(sl.begin(), nullptr);
+  // Double check, if data still here, after sl clear
+  ASSERT_EQ(moved_sl.size(), size);
+  ASSERT_EQ(&moved_sl.front(), &front);
+  ASSERT_EQ(&moved_sl.back(), &back);
+  ASSERT_EQ(moved_sl.begin(), begin);
+  it = moved_sl.begin();
+  for (int i = -5; i <= 5; ++i, ++it) {
+    ASSERT_NE(it, nullptr);
+    ASSERT_EQ(*it, i);
+  }
+}
+
+TEST(Initialization, ConstructorsInitList) {
+  skip_list<int> sl{1, -1, 2};
+  ASSERT_EQ(sl.front(), -1);
+  ASSERT_EQ(sl.back(), 2);
+  ASSERT_EQ(*(++sl.begin()), 1);
+  skip_list<int> sl1{0, 1, 2, -1, -2};
+  auto it = sl1.cbegin();
+  for (int i = -2; i <= 2; ++i, ++it) {
+    ASSERT_NE(it, nullptr);
+    ASSERT_EQ(*it, i);
+  }
 }
